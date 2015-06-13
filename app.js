ffa8ae4bd700ab8528f1a28a20e404c57879f050
Nathan West <Lucretiel@gmail.com>
2015-06-13 14:06:54 -0400

    Added other measures of time

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 (function () {
 	angular.module("countdownApp", []).controller('CountdownController', function ($scope, $interval) {
 		var beachWeekDate = new Date(2015, 06, 5, 16);
-		var beachWeekTime = beachWeekDate.getTime() / 1000.0;
+		var beachWeekTime = beachWeekDate.getTime();
+		$scope.countdowns = {}
 
 		$interval(function () {
 			var nowDate = new Date();
-			var nowTime = nowDate.getTime() / 1000.0;
-			$scope.timeRemaining = beachWeekTime - nowTime;
+			var nowTime = nowDate.getTime();
+			var timeRemaining = beachWeekTime - nowTime;
+
+			var seconds = $scope.seconds = timeRemaining / 1000;
+			var minutes = $scope.minutes = seconds / 60;
+			var hours = $scope.hours = minutes / 60;
+			var days = $scope.days = hours / 24;
+			var weeks = $scope.weeks = days / 7;
 		}, 1);
 	});
 })();
