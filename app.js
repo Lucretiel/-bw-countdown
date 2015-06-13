346505a72af386c0ddd0c3b09e12d14fb22c1c45
Nathan West <Lucretiel@gmail.com>
2015-06-13 01:16:56 -0400

    Add some Javascript

---
+++ app.js
@@ -0,0 +1,13 @@
+(function () {
+	angular.module("countdownApp")
+	.controller('CountdownController', function ($scope, $interval) {
+		var beachWeekDate = new Date(2015, 07, 5, 16);
+		var beachWeekTime = beachWeekDate.getTime() / 1000.0;
+
+		$interval(function () {
+			var nowDate = new Date()
+			var nowTime = nowDate.getTime() / 1000.0
+			$scope.timeRetimeRemaining = beachWeekTime - nowTime
+		}, 1);
+	});
+})();
