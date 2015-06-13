c0c5bb80f3b4761ca2ccc621f8091522c45796de
Nathan West <Lucretiel@gmail.com>
2015-06-13 01:35:48 -0400

    Javascript months are aparently 0-indexed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function () {
 	angular.module("countdownApp", []).controller('CountdownController', function ($scope, $interval) {
-		var beachWeekDate = new Date(2015, 07, 5, 16);
+		var beachWeekDate = new Date(2015, 06, 5, 16);
 		var beachWeekTime = beachWeekDate.getTime() / 1000.0;
 
 		$interval(function () {
